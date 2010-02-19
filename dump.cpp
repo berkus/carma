d89ca05ff1cb6090d36a4889730934fa370168f2
Berkus <berkus@exquance.com>
2010-02-19 02:52:12 +0200

    Dump vertices of a mesh.

--- dump.cpp
+++ dump.cpp
@@ -1,5 +1,11 @@
 #include "blocks.h"
+#include <stdio.h>
 
 void mesh_t::dump()
 {
+    // Print vertices.
+    for (size_t i = 0; i < vertices.size(); i++)
+    {
+        printf("Vertex{%f,%f,%f}\n", vertices[i].x, vertices[i].y, vertices[i].z);
+    }
 }
