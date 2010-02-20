7500fe7d23e8737ebc3b16a91d3e6f5cbee13802
Berkus <berkus@exquance.com>
2010-02-20 20:23:47 +0200

    Simplify code using helper classes.

--- model.cpp
+++ model.cpp
@@ -4,40 +4,26 @@
 using namespace std;
 using raii_wrapper::file;
 
-unsigned char file_header[16];   //      The file header
-
 int main(int argc, char **argv)
 {
-    size_t count;
-
     if (argc < 2)       //Was a file name specified?
     {
-        printf("\n\n ERROR!!!   ");
-        puts("File name required\n");
-        return(1);    //if not, exit
+        printf("\n\n ERROR!!! File name required\n\n");
+        return 1;
     }
 
     file f(argv[1], ios::in|ios::binary);
 
-    count = f.read(file_header, 16);        //Read 16 byte file header
-
-    if(count < 16)
+    if (!resource_file_t::read_file_header(f))
     {
-        puts("\n\n ERROR!!!  File header truncated.\n");     //exit if file header short
-        return(1);
+        printf("\n\n ERROR!!! File header truncated.\n\n");     //exit if file header short
+        return 1;
     }
-
-    printf("File header Data: ");      //Print file header to the screen
-    for(int loop=0;loop<16;loop++)
-    {
-        printf("%02hX ",(file_header[loop]));
-    }
-    puts("\nReading Chunks:");
 
     mesh_t mesh;
 
     while (mesh.read(f))
         mesh.dump();
 
-    return(0);                         //Exit program
+    return 0;
 }
--- pixdec.cpp
+++ pixdec.cpp
@@ -1,16 +1,11 @@
 #include "blocks.h"
 #include <cstdio>
-#include <cstring>
 
 using namespace std;
 using namespace raii_wrapper;
 
-unsigned char file_header[16];
-
 int main(int argc, char **argv)
 {
-    size_t count;
-
     if (argc < 2)
     {
         printf("\n\n ERROR!!! File name required.\n\n");
@@ -18,64 +13,17 @@ int main(int argc, char **argv)
     }
 
     file f(argv[1], ios::in|ios::binary);
-    filebinio fio(f);
 
-    count = f.read(file_header, 16);        //Read 16 byte file header
-
-    if(count < 16)
+    if (!resource_file_t::read_file_header(f))
     {
-        printf("\n\n ERROR!!! File header truncated.\n");
+        printf("\n\n ERROR!!! File header truncated.\n\n");     //exit if file header short
         return 1;
     }
 
-    printf("File header Data: ");      //Print file header to the screen
-    for(int loop=0;loop<16;loop++)
-    {
-        printf("%02hX ",(file_header[loop]));
-    }
-    puts("\nReading Chunks:");
+    pixelmap_t pixelmap;
 
-    chunk_header_t ch;
-
-    ch.read(f);
-    uint8_t marker;
-    uint16_t w, h, what, use_w, use_h;
-    string str;
-    fio.read8(marker);
-    fio.read16be(w);
-    fio.read16be(use_w);
-    fio.read16be(h);
-    fio.read16be(use_h);
-    fio.read16be(what);
-    chunk_header_t::read_c_string(f, str);
-
-    printf("Pixmap header chunk [type %02x, size %d]: %s (%d, %d[%d] x %d[%d], %d)\n", ch.type, ch.size, str.c_str(), marker, w, use_w, h, use_h, what);
-
-    ch.read(f);
-    uint32_t payload_size, whatnot;
-    fio.read32be(payload_size);
-    fio.read32be(whatnot);
-
-    printf("Pixmap data chunk [type %02x, size %d]: payload %d, whatnot %d\n", ch.type, ch.size, payload_size, whatnot);
-
-    char* buf = new char [payload_size];
-    f.read(buf, payload_size);
-
-    const char* arr = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.,_!@#$%^&*()_+";
-    size_t sz = strlen(arr);
-
-    for (int y = 0; y < h; y++)
-    {
-        for (int x = 0; x < use_w; x++)
-        {
-            printf("%c", arr[buf[y*w+x] % sz]);
-        }
-        printf("\n");
-    }
-//     pixmap_t pixelmap;
-
-//     while (pixelmap.read(f))
-//         pixelmap.dump();
+    while (pixelmap.read(f))
+        pixelmap.dump();
 
     return 0;
 }
