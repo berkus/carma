fe148acb383443b53f750b12b2d1d3bc9d889804
Berkus <berkus@exquance.com>
2010-02-23 20:56:31 +0200

    Calc normals only once. Load textures. Init GL context early.

--- glook.cpp
+++ glook.cpp
@@ -1,3 +1,11 @@
+//
+// Part of Roadkill Project. Check http://<urlhere> for latest version.
+//
+// Copyright 2010, Stanislav Karchebnyy <berkus@exquance.com>
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
+//
 #include <GL/glut.h>
 #include <cstdlib>
 #include <cstring>
@@ -6,6 +14,9 @@
 #include <math.h>
 #include "blocks.h"
 #include "texturizer.h"
+#include <algorithm>
+#include <ctype.h>
+#include "math/matrix.h"
 
 #define WIDTH 800
 #define HEIGHT 600
@@ -21,10 +32,6 @@
 using namespace std;
 using namespace raii_wrapper;
 
-#define NCOLORS 7
-static GLfloat colors[][3] = { {1.0f, 1.0f, 1.0f}, {1.0f, 1.0f, 0.0f}, {1.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 1.0f},
-                        {1.0f, 0.0f, 0.0f}, {0.0f, 1.0f, 0.0f}, {0.0f, 0.0f, 1.0f} };
-
 static GLfloat LightPos[4]={-5.0f,5.0f,10.0f,0.0f};
 static GLfloat Ambient[4]={0.5f,0.5f,0.5f,1.0f};
 
@@ -38,60 +45,54 @@ static int prevTime;
 static GLfloat rot = 0.0f;
 
 // Calculate normal from vertices in counter-clockwise order.
-static vertex_t calc_normal(vertex_t v1, vertex_t v2, vertex_t v3)
+template <typename type_t>
+static vector_t<type_t> calc_normal(vector_t<type_t> v1, vector_t<type_t> v2, vector_t<type_t> v3)
 {
-    double v1x,v1y,v1z,v2x,v2y,v2z;
-    double nx,ny,nz;
-    double vLen;
+    return normalize((v1 - v2) & (v2 - v3));
+}
 
-    vertex_t Result;
+void mesh_t::calc_normals()
+{
+    normals.clear();
+    for (size_t n = 0; n < mesh.vertices.size(); n++)
+    {
+        normals.push_back(vector_t<float>());
+    }
 
-    // Calculate vectors
-    v1x = v1.x - v2.x;
-    v1y = v1.y - v2.y;
-    v1z = v1.z - v2.z;
-
-    v2x = v2.x - v3.x;
-    v2y = v2.y - v3.y;
-    v2z = v2.z - v3.z;
-
-    // Get cross product of vectors
-    nx = (v1y * v2z) - (v1z * v2y);
-    ny = (v1z * v2x) - (v1x * v2z);
-    nz = (v1x * v2y) - (v1y * v2x);
-
-    // Normalise final vector
-    vLen = sqrt( (nx * nx) + (ny * ny) + (nz * nz) );
-
-    Result.x = (float)(nx / vLen);
-    Result.y = (float)(ny / vLen);
-    Result.z = (float)(nz / vLen);
-
-    return Result;
+    for (size_t n = 0; n < mesh.faces.size(); n++)
+    {
+        vector_t<float> normal = calc_normal(mesh.vertices[mesh.faces[n].v1], mesh.vertices[mesh.faces[n].v2], mesh.vertices[mesh.faces[n].v3]);
+        normals[mesh.faces[n].v1] = normals[mesh.faces[n].v2] = normals[mesh.faces[n].v3] = normal;
+    }
 }
 
 static void render()        /* function called whenever redisplay needed */
 {
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);     /* clear the display */
+//     glEnable(GL_TEXTURE_2D);
 
     glLoadIdentity();
     glTranslatef(0.0f, 0.0f, -3.0f);
     glRotatef(45.0f, 1.0f, 0.0f, 0.0f);
     glRotatef(rot, 0.0f, 1.0f, 0.0f);
 
+//     texturizer.draw_texture("SCWHEEL.PIX");
     glBegin(GL_TRIANGLES);
     // Draw all faces of the mesh
     for (size_t n = 0; n < mesh.faces.size(); n++)
     {
-        vertex_t normal = calc_normal(mesh.vertices[mesh.faces[n].v1], mesh.vertices[mesh.faces[n].v2], mesh.vertices[mesh.faces[n].v3]);
-        glNormal3f(normal.x, normal.y, normal.z);
+//         texturizer.set_texture(mesh.materials[mesh.faces[n].material_id - 1]);
 
-        glColor3fv(colors[(mesh.faces[n].material_id - 1) % NCOLORS]);// to be replaced with textures
-//         glTexCoord2f(uv);
+        glNormal3f(mesh.normals[mesh.faces[n].v1].x, mesh.normals[mesh.faces[n].v1].y, mesh.normals[mesh.faces[n].v1].z);
+        glTexCoord2f(mesh.uvcoords[mesh.faces[n].v1].u, mesh.uvcoords[mesh.faces[n].v1].v);
         glVertex3f(mesh.vertices[mesh.faces[n].v1].x, mesh.vertices[mesh.faces[n].v1].y, mesh.vertices[mesh.faces[n].v1].z);
-//         glTexCoord2f(uv);
+
+        glNormal3f(mesh.normals[mesh.faces[n].v2].x, mesh.normals[mesh.faces[n].v2].y, mesh.normals[mesh.faces[n].v2].z);
+        glTexCoord2f(mesh.uvcoords[mesh.faces[n].v2].u, mesh.uvcoords[mesh.faces[n].v2].v);
         glVertex3f(mesh.vertices[mesh.faces[n].v2].x, mesh.vertices[mesh.faces[n].v2].y, mesh.vertices[mesh.faces[n].v2].z);
-//         glTexCoord2f(uv);
+
+        glNormal3f(mesh.normals[mesh.faces[n].v3].x, mesh.normals[mesh.faces[n].v3].y, mesh.normals[mesh.faces[n].v3].z);
+        glTexCoord2f(mesh.uvcoords[mesh.faces[n].v3].u, mesh.uvcoords[mesh.faces[n].v3].v);
         glVertex3f(mesh.vertices[mesh.faces[n].v3].x, mesh.vertices[mesh.faces[n].v3].y, mesh.vertices[mesh.faces[n].v3].z);
     }
     glEnd();
@@ -164,12 +165,29 @@ static bool load_textures(const char* fname, mesh_t& mesh)
 
     while (mat.read(f))
         materials[mat.name] = mat;
+    f.close();
+
+    // Load pixmaps from PIX file.
+    char* pixfile = pathsubst(fname, BASE_DIR"PIXELMAP/", ".PIX");
+    file pix(pixfile, ios::in|ios::binary);
+    free(pixfile);
+    CHECK_READ(texturizer.read(pix));
+    pix.close();
 
     for (size_t i = 0; i < mesh.materials.size(); i++)
     {
         printf("Loading material %s...", mesh.materials[i].c_str());
         if (materials.find(mesh.materials[i]) != materials.end())
-            printf("FOUND\n");
+        {
+            std::string& pixmap = materials[mesh.materials[i]].pixelmap_name;
+//             std::transform(pixmap.begin(), pixmap.end(), pixmap.begin(), toupper);
+            printf("FOUND, binding texture %s\n", pixmap.c_str());
+            if (!texturizer.set_texture(pixmap))
+            {
+                printf("Couldn't generate OpenGL texture!\n");
+                return false;
+            }
+        }
         else
             printf("NOT FOUND\n");
     }
@@ -248,14 +266,19 @@ int main(int argc, char *argv[])
 {
     int win;
 
-    glutInit(&argc, argv);        /* initialize GLUT system */
-
-    if (argc < 2)       //Was a file name specified?
+    if (argc < 2)
     {
-        printf("\n\n ERROR!!!   ");
-        puts("File name required\n");
+        printf("\n\n ERROR!!! File name required\n\n");
         return 1;
     }
+
+    glutInit(&argc, argv);        /* initialize GLUT system */
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(WIDTH, HEIGHT);
+    win = glutCreateWindow("Glook");   /* create window */
+    /* from this point on the current window is win */
+
+    init(WIDTH, HEIGHT);
 
     try {
         if (!load_mesh(argv[1]))
@@ -263,7 +286,6 @@ int main(int argc, char *argv[])
             printf("Mesh load failed!\n");
             return 1;
         }
-        mesh.dump();
 
         if (!load_textures(argv[1], mesh))
         {
@@ -277,12 +299,8 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(WIDTH, HEIGHT);
-    win = glutCreateWindow("Glook");   /* create window */
-    /* from this point on the current window is win */
-
-    init(WIDTH, HEIGHT);
+    printf("Files loaded.\n");
+    mesh.calc_normals();
 
     glutDisplayFunc(render);
     glutKeyboardFunc(key);
