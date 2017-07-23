776d6a7d069646ffc5c359aab313bf9d7ce96ba2
Berkus Decker <berkus@madfire.net>
2017-07-23 19:14:23 +0300

    Change base dir

--- loader.h
+++ loader.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define BASE_DIR "DecodedData/DATA/"
+#define BASE_DIR "../DecodedData/DATA/" // @todo Remove this completely... use std::filesystem
 
 bool load_actor(const char* fname);
