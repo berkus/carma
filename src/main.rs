0103c83813e8a1b6beb51b88767ef605fd1990dd
Berkus Decker <berkus@madfire.net>
2017-07-28 01:45:16 +0300

    Enable backface culling

--- src/main.rs
+++ src/main.rs
@@ -148,6 +148,7 @@ fn main()
                 write: true,
                 .. Default::default()
             },
+            backface_culling: glium::draw_parameters::BackfaceCullingMode::CullClockwise,
             .. Default::default()
         };
 
