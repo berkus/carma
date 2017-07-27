814ddefcf73778162e111e086e63711a759b9f0d
Berkus Decker <berkus@madfire.net>
2017-07-28 01:50:19 +0300

    Add camera

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,40 @@ use carma::teapot;
 
 // implement_vertex!(Vertex, position, tex_coords);
 
+fn view_matrix(position: &[f32; 3], direction: &[f32; 3], up: &[f32; 3]) -> [[f32; 4]; 4] {
+    let f = {
+        let f = direction;
+        let len = f[0] * f[0] + f[1] * f[1] + f[2] * f[2];
+        let len = len.sqrt();
+        [f[0] / len, f[1] / len, f[2] / len]
+    };
+
+    let s = [up[1] * f[2] - up[2] * f[1],
+             up[2] * f[0] - up[0] * f[2],
+             up[0] * f[1] - up[1] * f[0]];
+
+    let s_norm = {
+        let len = s[0] * s[0] + s[1] * s[1] + s[2] * s[2];
+        let len = len.sqrt();
+        [s[0] / len, s[1] / len, s[2] / len]
+    };
+
+    let u = [f[1] * s_norm[2] - f[2] * s_norm[1],
+             f[2] * s_norm[0] - f[0] * s_norm[2],
+             f[0] * s_norm[1] - f[1] * s_norm[0]];
+
+    let p = [-position[0] * s_norm[0] - position[1] * s_norm[1] - position[2] * s_norm[2],
+             -position[0] * u[0] - position[1] * u[1] - position[2] * u[2],
+             -position[0] * f[0] - position[1] * f[1] - position[2] * f[2]];
+
+    [
+        [s[0], u[0], f[0], 0.0],
+        [s[1], u[1], f[1], 0.0],
+        [s[2], u[2], f[2], 0.0],
+        [p[0], p[1], p[2], 1.0],
+    ]
+}
+
 fn main()
 {
     use glium::{glutin, Surface};
@@ -54,12 +88,14 @@ fn main()
 
         out vec3 v_normal;
 
-        uniform mat4 matrix;
         uniform mat4 perspective;
+        uniform mat4 view;
+        uniform mat4 model;
 
         void main() {
-            v_normal = transpose(inverse(mat3(matrix))) * normal;
-            gl_Position = perspective * matrix * vec4(position, 1.0);
+            mat4 modelview = view * model;
+            v_normal = transpose(inverse(mat3(modelview))) * normal;
+            gl_Position = perspective * modelview * vec4(position, 1.0);
         }
     "#;
 
@@ -124,6 +160,8 @@ fn main()
             ]
         };
 
+        let view = view_matrix(&[2.0, -1.0, 1.0], &[-2.0, 1.0, 1.0], &[0.0, 1.0, 0.0]);
+
         let uniforms = uniform! {
             // matrix: [
             //     [ t.cos()*0.01, t.sin(), 0.0, 0.0],
@@ -132,14 +170,15 @@ fn main()
             //     [0.0, 0.0, 0.0, 1.0f32],
             // ],
             // tex: &texture,
-            matrix: [
+            model: [
                 [0.01, 0.0, 0.0, 0.0],
                 [0.0, 0.01, 0.0, 0.0],
                 [0.0, 0.0, 0.01, 0.0],
                 [0.0, 0.0, 2.0, 1.0f32]
             ],
-            u_light: light,
+            view: view,
             perspective: perspective,
+            u_light: light,
         };
 
         let params = glium::DrawParameters {
