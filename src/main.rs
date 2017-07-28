bf6ecb9699e51ce1a6a1158bf344dc0e0e5301c6
Berkus Decker <berkus@madfire.net>
2017-07-28 08:51:08 +0300

    Add specular highlight

--- src/main.rs
+++ src/main.rs
@@ -87,6 +87,7 @@ fn main()
         in vec3 normal;
 
         out vec3 v_normal;
+        out vec3 v_position;
 
         uniform mat4 perspective;
         uniform mat4 view;
@@ -96,6 +97,7 @@ fn main()
             mat4 modelview = view * model;
             v_normal = transpose(inverse(mat3(modelview))) * normal;
             gl_Position = perspective * modelview * vec4(position, 1.0);
+            v_position = gl_Position.xyz / gl_Position.w;
         }
     "#;
 
@@ -107,16 +109,24 @@ fn main()
         #version 140
 
         in vec3 v_normal;
+        in vec3 v_position;
 
         out vec4 color;
 
         uniform vec3 u_light;
 
+        const vec3 ambient_color = vec3(0.2, 0.0, 0.0);
+        const vec3 diffuse_color = vec3(0.6, 0.0, 0.0);
+        const vec3 specular_color = vec3(1.0, 1.0, 1.0);
+
         void main() {
-            float brightness = dot(normalize(v_normal), normalize(u_light));
-            vec3 dark_color = vec3(0.6, 0.0, 0.0);
-            vec3 regular_color = vec3(1.0, 0.0, 0.0);
-            color = vec4(mix(dark_color, regular_color, brightness), 1.0);
+            float diffuse = max(dot(normalize(v_normal), normalize(u_light)), 0.0);
+
+            vec3 camera_dir = normalize(-v_position);
+            vec3 half_direction = normalize(normalize(u_light) + camera_dir);
+            float specular = pow(max(dot(half_direction, normalize(v_normal)), 0.0), 16.0);
+
+            color = vec4(ambient_color + diffuse * diffuse_color + specular * specular_color, 1.0);
         }
     "#;
 
