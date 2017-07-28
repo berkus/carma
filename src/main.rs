c183ba054a49520440599443a78e7149ed4baa5f
Berkus Decker <berkus@madfire.net>
2017-07-28 10:02:12 +0300

    Add normal mapping

--- src/main.rs
+++ src/main.rs
@@ -51,17 +51,7 @@ fn view_matrix(position: &[f32; 3], direction: &[f32; 3], up: &[f32; 3]) -> [[f3
 fn main()
 {
     use glium::{glutin, Surface};
-
     use std::io::Cursor;
-    // let image = image::load(Cursor::new(&include_bytes!("in-the-name-of.png")[..]),
-    //                         image::PNG).unwrap().to_rgba();
-    // let image_dimensions = image.dimensions();
-    // let image = glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
-
-    let image = image::load(Cursor::new(&include_bytes!("tuto-14-diffuse.jpg")[..]),
-                            image::JPEG).unwrap().to_rgba();
-    let image_dimensions = image.dimensions();
-    let image = glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
 
     let mut events_loop = glutin::EventsLoop::new();
     let window = glutin::WindowBuilder::new()
@@ -72,16 +62,17 @@ fn main()
 
     let display = glium::Display::new(window, context, &events_loop).unwrap();
 
-    // let vertex1 = Vertex { position: [-0.5, -0.5], tex_coords: [0.0, 0.0] };
-    // let vertex2 = Vertex { position: [ 0.0,  0.5], tex_coords: [0.0, 1.0] };
-    // let vertex3 = Vertex { position: [ 0.5, -0.25], tex_coords: [1.0, 0.0] };
-    // let shape = vec![vertex1, vertex2, vertex3];
-
-    // let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
-    // let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
-
-    // let texture = glium::texture::Texture2d::new(&display, image).unwrap();
+    let image = image::load(Cursor::new(&include_bytes!("tuto-14-diffuse.jpg")[..]),
+                            image::JPEG).unwrap().to_rgba();
+    let image_dimensions = image.dimensions();
+    let image = glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
     let diffuse_texture = glium::texture::SrgbTexture2d::new(&display, image).unwrap();
+
+    let image = image::load(Cursor::new(&include_bytes!("tuto-14-normal.png")[..]),
+                            image::PNG).unwrap().to_rgba();
+    let image_dimensions = image.dimensions();
+    let image = glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
+    let normal_map = glium::texture::Texture2d::new(&display, image).unwrap();
 
     let vertex_shader_src = r#"
         #version 150
@@ -118,18 +109,39 @@ fn main()
 
         uniform vec3 u_light;
         uniform sampler2D diffuse_tex;
+        uniform sampler2D normal_tex;
 
         const vec3 specular_color = vec3(1.0, 1.0, 1.0);
+
+        mat3 cotangent_frame(vec3 normal, vec3 pos, vec2 uv) {
+            vec3 dp1 = dFdx(pos);
+            vec3 dp2 = dFdy(pos);
+            vec2 duv1 = dFdx(uv);
+            vec2 duv2 = dFdy(uv);
+
+            vec3 dp2perp = cross(dp2, normal);
+            vec3 dp1perp = cross(normal, dp1);
+            vec3 T = dp2perp * duv1.x + dp1perp * duv2.x;
+            vec3 B = dp2perp * duv1.y + dp1perp * duv2.y;
+
+            float invmax = inversesqrt(max(dot(T, T), dot(B, B)));
+            return mat3(T * invmax, B * invmax, normal);
+        }
 
         void main() {
             vec3 diffuse_color = texture(diffuse_tex, v_tex_coords).rgb;
             vec3 ambient_color = diffuse_color * 0.2;
 
-            float diffuse = max(dot(normalize(v_normal), normalize(u_light)), 0.0);
+            vec3 normal_map = texture(normal_tex, v_tex_coords).rgb;
+
+            mat3 tbn = cotangent_frame(v_normal, v_position, v_tex_coords);
+            vec3 real_normal = normalize(tbn * -(normal_map * 2.0 - 1.0));
+
+            float diffuse = max(dot(normalize(real_normal), normalize(u_light)), 0.0);
 
             vec3 camera_dir = normalize(-v_position);
             vec3 half_direction = normalize(normalize(u_light) + camera_dir);
-            float specular = pow(max(dot(half_direction, normalize(v_normal)), 0.0), 16.0);
+            float specular = pow(max(dot(half_direction, normalize(real_normal)), 0.0), 16.0);
 
             color = vec4(ambient_color + diffuse * diffuse_color + specular * specular_color, 1.0);
         }
