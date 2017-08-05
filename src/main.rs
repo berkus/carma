c552cb7b2a6c7cfa76ed42cb12637e5e85142c46
Berkus Decker <berkus@madfire.net>
2017-08-05 10:49:03 +0300

    Pass specular color through uniforms

--- src/main.rs
+++ src/main.rs
@@ -81,10 +81,9 @@ fn main()
         out vec4 color;
 
         uniform vec3 u_light;
+        uniform vec3 u_specular_color;
         uniform sampler2D diffuse_tex;
         uniform sampler2D normal_tex;
-
-        const vec3 specular_color = vec3(1.0, 1.0, 1.0);
 
         mat3 cotangent_frame(vec3 normal, vec3 pos, vec2 uv) {
             vec3 dp1 = dFdx(pos);
@@ -117,7 +116,7 @@ fn main()
             vec3 half_direction = normalize(normalize(u_light) + camera_dir);
             float specular = pow(max(dot(half_direction, normalize(real_normal)), 0.0), 16.0);
 
-            color = vec4(ambient_color + diffuse * diffuse_color + specular * specular_color, 1.0);
+            color = vec4(ambient_color + diffuse * diffuse_color + specular * u_specular_color, 1.0);
         }
     "#;
 
@@ -160,6 +159,7 @@ fn main()
             view: camera.get_view(),
             perspective: camera.get_perspective(),
             u_light: light,
+            u_specular_color: [1.0, 1.0, 1.0f32],
             diffuse_tex: &diffuse_texture,
             normal_tex: &normal_map,
         };
