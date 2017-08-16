cd2b54f172e47df103ccd7aeffa111c70258458f
Berkus Decker <berkus@madfire.net>
2017-08-16 09:36:22 +0200

    Add some code license headers

--- src/support/camera.rs
+++ src/support/camera.rs
@@ -1,5 +1,11 @@
-// extern crate glutin;
-// extern crate glium;
+//
+// Part of Roadkill Project.
+//
+// Copyright 2010, 2017, Stanislav Karchebnyy <berkus@madfire.net>
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
+//
 
 use glium::glutin;
 
--- src/support/texture.rs
+++ src/support/texture.rs
@@ -1,3 +1,12 @@
+//
+// Part of Roadkill Project.
+//
+// Copyright 2010, 2017, Stanislav Karchebnyy <berkus@madfire.net>
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
 // Pixmap consists of two chunks: name and data
 // TODO: use shared_data_t for pixmap contents to avoid copying.
 #[derive(Default)]
