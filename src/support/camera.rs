72c8b3bb8cac837ed76c5fbc84708dbe59f4076b
Berkus Decker <berkus@madfire.net>
2017-08-29 02:43:18 +0300

    Convert camera ops to cgmath. Add camera rotation Left/Right
    Make camera fov and zplanes tunable

--- src/support/camera.rs
+++ src/support/camera.rs
@@ -6,13 +6,16 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
 //
-
+use cgmath::*;
 use glium::glutin;
 
 pub struct CameraState {
     aspect_ratio: f32,
-    position: (f32, f32, f32),
-    direction: (f32, f32, f32),
+    fov: f32,
+    zfar: f32,
+    znear: f32,
+    position: Point3<f32>,
+    direction: Vector3<f32>,
 
     moving_up: bool,
     moving_left: bool,
@@ -20,160 +23,115 @@ pub struct CameraState {
     moving_right: bool,
     moving_forward: bool,
     moving_backward: bool,
+    rotating_left: bool,
+    rotating_right: bool,
 }
 
 impl CameraState {
     pub fn new() -> CameraState {
         CameraState {
             aspect_ratio: 1024.0 / 768.0,
-            position: (0.1, 0.1, 1.0),
-            direction: (0.0, 0.0, -1.0),
+            fov: 90.0,
+            zfar: 100.0,
+            znear: 0.1,
+            position: Point3 {
+                x: 0.1,
+                y: 0.1,
+                z: 1.0,
+            },
+            direction: Vector3 {
+                x: 0.0,
+                y: 0.0,
+                z: -1.0,
+            },
             moving_up: false,
             moving_left: false,
             moving_down: false,
             moving_right: false,
             moving_forward: false,
             moving_backward: false,
+            rotating_left: false,
+            rotating_right: false,
         }
     }
 
     pub fn set_position(&mut self, pos: (f32, f32, f32)) {
-        self.position = pos;
+        self.position = Point3::from(pos);
     }
 
     pub fn set_direction(&mut self, dir: (f32, f32, f32)) {
-        self.direction = dir;
+        self.direction = Vector3::from(dir);
     }
 
     pub fn set_aspect_ratio(&mut self, aspect: f32) {
         self.aspect_ratio = aspect;
     }
 
-    #[cfg_attr(rustfmt, rustfmt_skip)]
     pub fn get_perspective(&self) -> [[f32; 4]; 4] {
-        // viewport_t() : fovy(45.0f), znear(0.1f), zfar(100.0f), w(0), h(0) {}
-
-        let fov: f32 = 3.141592 / 2.0;
-        let zfar = 1024.0;
-        let znear = 0.001;
-
-        let f = 1.0 / (fov / 2.0).tan();
-
-        // note: remember that this is column-major, so the lines of code are actually columns
-        [
-            [f / self.aspect_ratio,    0.0,              0.0              ,   0.0],
-            [         0.0         ,     f ,              0.0              ,   0.0],
-            [         0.0         ,    0.0,  (zfar+znear)/(zfar-znear)    ,   1.0],
-            [         0.0         ,    0.0, -(2.0*zfar*znear)/(zfar-znear),   0.0],
-        ]
+        perspective(Deg(self.fov), self.aspect_ratio, self.znear, self.zfar).into()
     }
 
     pub fn get_view(&self) -> [[f32; 4]; 4] {
-        let f = {
-            let f = self.direction;
-            let len = f.0 * f.0 + f.1 * f.1 + f.2 * f.2;
-            let len = len.sqrt();
-            (f.0 / len, f.1 / len, f.2 / len)
+        let up = Vector3 {
+            x: 0.0,
+            y: 1.0,
+            z: 0.0,
         };
-
-        let up = (0.0, 1.0, 0.0);
-
-        let s = (
-            f.1 * up.2 - f.2 * up.1,
-            f.2 * up.0 - f.0 * up.2,
-            f.0 * up.1 - f.1 * up.0,
-        );
-
-        let s_norm = {
-            let len = s.0 * s.0 + s.1 * s.1 + s.2 * s.2;
-            let len = len.sqrt();
-            (s.0 / len, s.1 / len, s.2 / len)
-        };
-
-        let u = (
-            s_norm.1 * f.2 - s_norm.2 * f.1,
-            s_norm.2 * f.0 - s_norm.0 * f.2,
-            s_norm.0 * f.1 - s_norm.1 * f.0,
-        );
-
-        let p = (
-            -self.position.0 * s.0 - self.position.1 * s.1 - self.position.2 * s.2,
-            -self.position.0 * u.0 - self.position.1 * u.1 - self.position.2 * u.2,
-            -self.position.0 * f.0 - self.position.1 * f.1 - self.position.2 * f.2,
-        );
-
-        // note: remember that this is column-major, so the lines of code are actually columns
-        // look-at matrix
-        [
-            [s_norm.0, u.0, f.0, 0.0],
-            [s_norm.1, u.1, f.1, 0.0],
-            [s_norm.2, u.2, f.2, 0.0],
-            [p.0, p.1, p.2, 1.0],
-        ]
+        Matrix4::look_at(self.position, Point3::from_vec(self.direction), up).into()
     }
 
     pub fn update(&mut self) {
-        let f = {
-            let f = self.direction;
-            let len = f.0 * f.0 + f.1 * f.1 + f.2 * f.2;
-            let len = len.sqrt();
-            (f.0 / len, f.1 / len, f.2 / len)
+        let f = self.direction.normalize();
+        let up = Vector3 {
+            x: 0.0,
+            y: 1.0,
+            z: 0.0,
         };
-
-        let up = (0.0, 1.0, 0.0);
-
-        let s = (
-            f.1 * up.2 - f.2 * up.1,
-            f.2 * up.0 - f.0 * up.2,
-            f.0 * up.1 - f.1 * up.0,
-        );
-
-        let s = {
-            let len = s.0 * s.0 + s.1 * s.1 + s.2 * s.2;
-            let len = len.sqrt();
-            (s.0 / len, s.1 / len, s.2 / len)
-        };
-
-        let u = (
-            s.1 * f.2 - s.2 * f.1,
-            s.2 * f.0 - s.0 * f.2,
-            s.0 * f.1 - s.1 * f.0,
-        );
+        let s = f.cross(up).normalize();
+        let u = s.cross(f);
 
         if self.moving_up {
-            self.position.0 += u.0 * 0.001;
-            self.position.1 += u.1 * 0.001;
-            self.position.2 += u.2 * 0.001;
+            self.position.x += u.x * 0.01;
+            self.position.y += u.y * 0.01;
+            self.position.z += u.z * 0.01;
         }
 
         if self.moving_left {
-            self.position.0 -= s.0 * 0.001;
-            self.position.1 -= s.1 * 0.001;
-            self.position.2 -= s.2 * 0.001;
+            self.position.x -= s.x * 0.01;
+            self.position.y -= s.y * 0.01;
+            self.position.z -= s.z * 0.01;
         }
 
         if self.moving_down {
-            self.position.0 -= u.0 * 0.001;
-            self.position.1 -= u.1 * 0.001;
-            self.position.2 -= u.2 * 0.001;
+            self.position.x -= u.x * 0.01;
+            self.position.y -= u.y * 0.01;
+            self.position.z -= u.z * 0.01;
         }
 
         if self.moving_right {
-            self.position.0 += s.0 * 0.001;
-            self.position.1 += s.1 * 0.001;
-            self.position.2 += s.2 * 0.001;
+            self.position.x += s.x * 0.01;
+            self.position.y += s.y * 0.01;
+            self.position.z += s.z * 0.01;
         }
 
         if self.moving_forward {
-            self.position.0 += f.0 * 0.001;
-            self.position.1 += f.1 * 0.001;
-            self.position.2 += f.2 * 0.001;
+            self.position.x += f.x * 0.01;
+            self.position.y += f.y * 0.01;
+            self.position.z += f.z * 0.01;
         }
 
         if self.moving_backward {
-            self.position.0 -= f.0 * 0.001;
-            self.position.1 -= f.1 * 0.001;
-            self.position.2 -= f.2 * 0.001;
+            self.position.x -= f.x * 0.01;
+            self.position.y -= f.y * 0.01;
+            self.position.z -= f.z * 0.01;
+        }
+
+        if self.rotating_left {
+            self.direction = Quaternion::from_angle_y(Deg(1.0)).rotate_vector(self.direction);
+        }
+
+        if self.rotating_right {
+            self.direction = Quaternion::from_angle_y(Deg(-1.0)).rotate_vector(self.direction);
         }
 
         // println!("Camera pos {:?} dir {:?}", self.position, self.direction);
@@ -192,6 +150,8 @@ impl CameraState {
         match key {
             glutin::VirtualKeyCode::Up => self.moving_up = pressed,
             glutin::VirtualKeyCode::Down => self.moving_down = pressed,
+            glutin::VirtualKeyCode::Left => self.rotating_left = pressed,
+            glutin::VirtualKeyCode::Right => self.rotating_right = pressed,
             glutin::VirtualKeyCode::A => self.moving_left = pressed,
             glutin::VirtualKeyCode::D => self.moving_right = pressed,
             glutin::VirtualKeyCode::W => self.moving_forward = pressed,
