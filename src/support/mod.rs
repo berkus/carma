371b5d8a2b40b64c3767176b20fc20bb2c39704a
Berkus Decker <berkus+github@metta.systems>
2020-08-28 02:25:29 +0300

    Drop old unused code

--- src/support/mod.rs
+++ src/support/mod.rs
@@ -143,49 +143,6 @@ pub fn path_subst(filepath: &Path, newdir: &Path, newext: Option<String>) -> Res
     Ok(dir)
 }
 
-// Returns a vertex buffer that should be rendered as `TrianglesList`.
-// pub fn load_wavefront(display: &Display, data: &[u8]) -> VertexBufferAny {
-//     #[derive(Copy, Clone)]
-//     struct Vertex {
-//         position: [f32; 3],
-//         normal: [f32; 3],
-//         texture: [f32; 2],
-//     }
-
-//     implement_vertex!(Vertex, position, normal, texture);
-
-//     let mut data = ::std::io::BufReader::new(data);
-//     let data = obj::Obj::load(&mut data);
-
-//     let mut vertex_data = Vec::new();
-
-//     for object in data.object_iter() {
-//         for shape in object.group_iter().flat_map(|g| g.indices().iter()) {
-//             match shape {
-//                 &genmesh::Polygon::PolyTri(genmesh::Triangle { x: v1, y: v2, z: v3 }) => {
-//                     for v in [v1, v2, v3].iter() {
-//                         let position = data.position()[v.0];
-//                         let texture = v.1.map(|index| data.texture()[index]);
-//                         let normal = v.2.map(|index| data.normal()[index]);
-
-//                         let texture = texture.unwrap_or([0.0, 0.0]);
-//                         let normal = normal.unwrap_or([0.0, 0.0, 0.0]);
-
-//                         vertex_data.push(Vertex {
-//                             position: position,
-//                             normal: normal,
-//                             texture: texture,
-//                         })
-//                     }
-//                 },
-//                 _ => unimplemented!()
-//             }
-//         }
-//     }
-
-//     glium::vertex::VertexBuffer::new(display, &vertex_data).unwrap().into_vertex_buffer_any()
-// }
-
 pub const NULL_CHUNK: u32 = 0x0;
 pub const PIXELMAP_HEADER_CHUNK: u32 = 0x3;
 pub const MATERIAL_DESC_CHUNK: u32 = 0x4;
