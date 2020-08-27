a6810935419569f3feb00de82b4465a8729773c9
Berkus Decker <berkus+github@metta.systems>
2020-08-28 02:24:35 +0300

    Start of bevy loader for assets

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
 //
+use bevy::asset::{AssetLoadRequestHandler, LoadRequest};
 use {
     crate::support::{camera::CameraState, car::Car, render_manager::RenderManager},
     anyhow::{anyhow, Context, Error, Result},
@@ -76,13 +77,34 @@ fn visit_files(dir: &Path, cb: &mut dyn for<'r> FnMut(&'r DirEntry) -> Result<()
     }
 }
 
-fn setup_textures(// mut commands: Commands,
-    // asset_server: Res<AssetServer>,
+struct CarHandler;
+
+impl AssetLoadRequestHandler for CarHandler {
+    fn handle_request(&self, load_request: &LoadRequest) {
+        let mut car = Car::load_from(load_request.path.clone()).unwrap();
+        unimplemented!()
+    }
+
+    fn extensions(&self) -> &[&str] {
+        &["ENC"]
+    }
+}
+
+fn setup_textures(
+    mut commands: Commands,
+    mut asset_server: ResMut<AssetServer>,
     // mut textures: ResMut<Assets<Texture>>,
     // mut texture_atlases: ResMut<Assets<TextureAtlas>>,
-) -> Result<Vec<Car>> {
+) /*-> Result<Vec<Car>>*/
+{
     // bevy @todo: load all textures into the texture atlas
     // TextureAtlasBuilder
+
+    // add handler for ENC assets, then
+    asset_server.add_handler(CarHandler);
+    asset_server
+        .load_asset_folder("DecodedData/DATA/CARS")
+        .unwrap();
 
     // let texture_handle = asset_server
     //     .load_sync(
@@ -96,33 +118,33 @@ fn setup_textures(// mut commands: Commands,
 
     // Load all cars and arrange in a grid 6x7 (40 cars total)
 
-    let mut cars = Vec::new();
-    let mut counter = 0;
-    visit_files(Path::new("DecodedData/DATA/CARS"), &mut |entry| {
-        if let Ok(file_type) = entry.file_type() {
-            let fname = entry
-                .path()
-                .to_str()
-                .map(String::from)
-                .ok_or_else(|| anyhow!("Failed to make filename"))?;
-            if file_type.is_file() && fname.ends_with(".ENC") {
-                let mut car = Car::load_from(fname)?;
-
-                let z = 1.0f32 * f32::from(counter / 7);
-                let x = 1.0f32 * f32::from(counter % 7 as u16);
-                counter += 1;
-
-                info!("Moving car {} to {},0,{}", counter, x, -z);
-
-                car.base_translation = Vector3::from([x, 0f32, -z]);
-
-                cars.push(car);
-            }
-        }
-        Ok(())
-    })?;
-
-    Ok(cars)
+    // let mut cars = Vec::new();
+    // let mut counter = 0;
+    // visit_files(Path::new("DecodedData/DATA/CARS"), &mut |entry| {
+    //     if let Ok(file_type) = entry.file_type() {
+    //         let fname = entry
+    //             .path()
+    //             .to_str()
+    //             .map(String::from)
+    //             .ok_or_else(|| anyhow!("Failed to make filename"))?;
+    //         if file_type.is_file() && fname.ends_with(".ENC") {
+    //             let mut car = Car::load_from(fname)?;
+    //
+    //             let z = 1.0f32 * f32::from(counter / 7);
+    //             let x = 1.0f32 * f32::from(counter % 7 as u16);
+    //             counter += 1;
+    //
+    //             info!("Moving car {} to {},0,{}", counter, x, -z);
+    //
+    //             car.base_translation = Vector3::from([x, 0f32, -z]);
+    //
+    //             cars.push(car);
+    //         }
+    //     }
+    //     Ok(())
+    // })?;
+    //
+    // Ok(cars)
 
     // commands
     //     .spawn(Camera2dComponents::default())
@@ -134,59 +156,60 @@ fn setup_textures(// mut commands: Commands,
     //     .with(Timer::from_seconds(0.1));
 }
 
-fn main() -> Result<()> {
+#[throws]
+fn main() {
     setup_logging().context("failed to initialize logging")?;
 
-    // App::build()
-    //     .add_default_plugins()
-    //     .add_startup_system(setup_textures.system())
-    //     .add_system(animate_camera.system())
-    //     .run();
-
-    let cars = setup_textures()?;
+    // let cars = setup_textures()?;
 
     // Prepare window -- move to bevy init
 
-    let mut events_loop = glium::glutin::event_loop::EventLoop::new();
-    let window = glium::glutin::window::WindowBuilder::new()
-        .with_title("carma")
-        .with_inner_size(glium::glutin::dpi::LogicalSize::new(800.0, 600.0));
-    let windowed_context = glium::glutin::ContextBuilder::new();
+    // let mut events_loop = glium::glutin::event_loop::EventLoop::new();
+    // let window = glium::glutin::window::WindowBuilder::new()
+    //     .with_title("carma")
+    //     .with_inner_size(glium::glutin::dpi::LogicalSize::new(800.0, 600.0));
+    // let windowed_context = glium::glutin::ContextBuilder::new();
+    //
+    // let display = glium::Display::new(window, windowed_context, &events_loop)?;
+    //
+    // let mut render_manager = RenderManager::new(&display);
+    // for car in &cars {
+    //     render_manager.prepare_car(car, &display);
+    // }
+    //
+    // let mut camera = CameraState::new();
 
-    let display = glium::Display::new(window, windowed_context, &events_loop)?;
+    App::build()
+        .add_default_plugins()
+        .add_startup_system(setup_textures.system())
+        // .add_system(animate_camera.system())
+        .run()
 
-    let mut render_manager = RenderManager::new(&display);
-    for car in &cars {
-        render_manager.prepare_car(car, &display);
-    }
-
-    let mut camera = CameraState::new();
-
-    events_loop.run(move |event, _, control_flow| {
-        println!("{:?}", event);
-        *control_flow = ControlFlow::Wait;
-
-        camera.update();
-
-        match event {
-            Event::LoopDestroyed => return,
-            Event::WindowEvent { event, .. } => match event {
-                // WindowEvent::Resized(physical_size) => display.resize(physical_size),
-                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-                _ => camera.process_input(&event),
-            },
-            Event::RedrawRequested(_) => {
-                let mut frame = display.draw();
-                frame.clear_color(0.4, 0.4, 0.4, 0.0);
-                frame.clear_depth(1.0);
-
-                for car in &cars {
-                    render_manager.draw_car(car, &mut frame, &camera);
-                }
-                frame.finish().unwrap();
-                // windowed_context.swap_buffers().unwrap();
-            }
-            _ => (),
-        }
-    });
+    // events_loop.run(move |event, _, control_flow| {
+    //     println!("{:?}", event);
+    //     *control_flow = ControlFlow::Wait;
+    //
+    //     camera.update();
+    //
+    //     match event {
+    //         Event::LoopDestroyed => return,
+    //         Event::WindowEvent { event, .. } => match event {
+    //             // WindowEvent::Resized(physical_size) => display.resize(physical_size),
+    //             WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+    //             _ => camera.process_input(&event),
+    //         },
+    //         Event::RedrawRequested(_) => {
+    //             let mut frame = display.draw();
+    //             frame.clear_color(0.4, 0.4, 0.4, 0.0);
+    //             frame.clear_depth(1.0);
+    //
+    //             for car in &cars {
+    //                 render_manager.draw_car(car, &mut frame, &camera);
+    //             }
+    //             frame.finish().unwrap();
+    //             // windowed_context.swap_buffers().unwrap();
+    //         }
+    //         _ => (),
+    //     }
+    // });
 }
