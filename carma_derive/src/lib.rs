238bd58dab105893f91f192fe248d402ab9bea1f
Berkus Decker <berkus+github@metta.systems>
2024-03-17 14:36:32 +0200

    wip: move derive macro to a local cargo package

--- carma_derive/src/lib.rs
+++ carma_derive/src/lib.rs
@@ -1,5 +1,3 @@
-#![crate_type = "proc-macro"]
-
 use {proc_macro::TokenStream, quote::quote, syn::DeriveInput};
 
 #[proc_macro_derive(ResourceTag)]
@@ -7,6 +5,7 @@ pub fn resource_tag_derive_macro(input: TokenStream) -> TokenStream {
     let ast: DeriveInput = syn::parse(input).unwrap();
     let name = &ast.ident;
     let gen = quote! {
+        use core::any::Any;
         impl ResourceTag for #name {
             fn as_any(&self) -> &dyn Any {
                 self
